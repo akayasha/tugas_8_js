e1953d175b68d2925364142d2b7d0eb136a72336
Firman Ismail <akayasha99@gmail.com>
2022-02-05 01:44:38 +0700

    tugas 8 js

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function objek(){
+  var bio = {
+    nama : "antonio",
+    umur : 25,
+    tb : 166,
+    hobi :"membaca"
+  }
+  console.log(bio)
+  console.log(bio.nama)
+  bio.hobi="gaming";
+  console.log(bio);
+}
+
+objek()
